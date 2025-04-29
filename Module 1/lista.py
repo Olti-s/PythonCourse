7aea92874da9e75c668798ab35eed060ba4cbd65
Olti-s <sogojevaolti451@gmail.com>
2025-04-29 17:43:58 +0200

    module 1
    python sintaksat basike llojet e te dhenave listat

---
+++ Module 1/lista.py
@@ -0,0 +1,42 @@
+name_of_list=["item1","item2","item3","item4","item5",]
+shopping_list=["banane",3.5,3]
+print(shopping_list[0])
+
+
+
+#lista1=["red","black","yellow","green"]
+
+ #lista1.append("blue") #shton element te ri
+
+#lista1.clear() fshin te gjitha elementete
+#lista1.count() #outputs the number of elements
+
+ #print(list1)
+
+lista2=name_of_list.copy() #copy from one list to thte other
+
+print(lista2)
+
+lista2.insert(1,"item10")
+print("lista 2",lista2)
+lista2.pop(1) #this remove the element form the specific place
+lista2.remove("item4") #remove element based on value
+lista2.reverse() # revereses the elements
+print(lista2[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
