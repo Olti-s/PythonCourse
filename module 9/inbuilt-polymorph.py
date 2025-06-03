6f9a180ac0c4dca97bbad812b054a86716fdf798
Olti-s <sogojevaolti451@gmail.com>
2025-06-03 17:43:41 +0200

    ..........

---
+++ module 9/inbuilt-polymorph.py
@@ -0,0 +1,33 @@
+string_length =len("Hello Olti")
+list_length =len([1,2,3,55,66])
+tuple_length =len((1,5,4,6,77,4))
+
+print(string_length)
+print(list_length)
+print(tuple_length)
+
+sum_of_list = sum([1,2,3,4,5])
+sum_of_tuple = sum((1,43,5))
+
+print(sum_of_tuple)
+print(sum_of_list)
+
+
+
+print(len("Hello"))          # Output: 5
+print(len([1, 2, 3]))        # Output: 3
+print(len({"a": 1, "b": 2})) # Output: 2
+
+
+print(str(123))        # Output: '123'
+print(str([1, 2, 3]))  # Output: '[1, 2, 3]'
+print(str({"a": 1}))   # Output: "{'a': 1}"
+
+
+print(type(123))        # Output: <class 'int'>
+print(type("Hello"))    # Output: <class 'str'>
+print(type([1, 2, 3]))  # Output: <class 'list'>
+
+
+
+
